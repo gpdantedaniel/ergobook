f54753a958896b3a3ce770f7fa0ec5160f722583
Dante Garcia <gp.dantedaniel@gmail.com>
2023-08-12 22:12:36 -0400

    Theme + toggle added

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,14 @@ import { BsPersonCircle } from 'react-icons/bs'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import ThemeButton from './ThemeButton'
 
 const Header = async () => {
   const supabase = createServerComponentClient({ cookies })
   const { data: { user } } = await supabase.auth.getUser()
 
   return (
-    <div className='fixed z-50 w-full h-16 flex justify-center border-b border-neutral-500 backdrop-blur-md bg-white/30'>
+    <div className='fixed z-50 w-full h-16 flex justify-center border-b border-neutral-500 backdrop-blur-md bg-white/30 dark:bg-neutral-700/30'>
       <div className='w-full md:w-10/12 px-4 md:px-0 flex items-center justify-start gap-3 md:gap-0 md:justify-between'>
         <div className='block md:hidden'>
           <AiOutlineMenu size={22}/>
@@ -19,6 +20,7 @@ const Header = async () => {
             {user?.email}
           </div>
           <BsPersonCircle size={22}/>
+          <ThemeButton/>
         </div>
       </div>
     </div>
