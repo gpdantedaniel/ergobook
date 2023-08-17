fad6a39c118fe902c4fc1f296d62e2ebe6394c4f
Dante Garcia <gp.dantedaniel@gmail.com>
2023-08-17 17:41:23 -0400

    SectionsBrowser now collapsible

--- components/SectionsBrowser.tsx
+++ components/SectionsBrowser.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import {
   DndContext,
@@ -28,8 +28,10 @@ import {
   reorderSections,
 } from "@/redux/sectionsSlice";
 import SectionCardCreate from "./Section/SectionCardCreate";
+import { BsLayoutSidebar } from "react-icons/bs";
 
 const SectionsBrowser = ({ notebook_id }: { notebook_id: number }) => {
+  const [closed, setClosed] = useState<boolean>(false);
   const loading = useSelector((state: RootState) => state.sections.loading);
   const sections = useSelector(sectionsSelectors.selectAll);
   const dispatch = useDispatch<AppDispatch>();
@@ -64,32 +66,82 @@ const SectionsBrowser = ({ notebook_id }: { notebook_id: number }) => {
   };
 
   return (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
+    <>
       <div
+        onClick={() => setClosed(false)}
         className="
-        theme-background 
-        theme-border border-r
-        w-64 resize-x
-        fixed border-box
-        top-16 bottom-0 left-0 z-10
-        overflow-y-auto
-        flex flex-col
+        theme-background
+        theme-border border
+        p-2 rounded-md
+        theme-text-colors
+        fixed top-20 left-4 z-10
+        hover:theme-background-hover
+        cursor-pointer
         "
       >
-        {!loading ? (
-          <SortableContext
-            items={sections}
-            strategy={verticalListSortingStrategy}
+        <BsLayoutSidebar size={18} />
+      </div>
+      <div
+        className={`
+        theme-border 
+        ${closed ? "border-none" : "border-r"}
+        ${closed ? "w-0" : "w-64"}
+        fixed border-box
+        top-16 bottom-0 left-0 z-10
+        overflow-x-hidden
+        flex flex-col
+        transition-width
+        duration-200
+        `}
+      >
+        <div
+          onClick={() => setClosed(true)}
+          className="
+          p-4 text-base 
+          theme-background 
+          theme-border border-b
+          cursor-pointer
+          flex items-center justify-between
+          "
+        >
+          Sections
+          <div
+            onClick={() => setClosed(false)}
+            className="
+            theme-background
+            theme-border border
+            p-2 rounded-md
+            theme-text-colors
+            hover:theme-background-hover
+            cursor-pointer
+            "
           >
-            {sections.map((section) => (
-              <SectionCard key={section.id} section={section} />
-            ))}
-            <SectionCardCreate notebook_id={notebook_id} />
-          </SortableContext>
+            <BsLayoutSidebar size={18} />
+          </div>
+        </div>
+        {!loading ? (
+          <div
+            className="
+            flex-1 h-full w-full
+            overflow-y-auto
+            "
+          >
+            <DndContext
+              sensors={sensors}
+              collisionDetection={closestCenter}
+              onDragEnd={handleDragEnd}
+            >
+              <SortableContext
+                items={sections}
+                strategy={verticalListSortingStrategy}
+              >
+                {sections.map((section) => (
+                  <SectionCard key={section.id} section={section} />
+                ))}
+                <SectionCardCreate notebook_id={notebook_id} />
+              </SortableContext>
+            </DndContext>
+          </div>
         ) : (
           <div
             className="
@@ -115,7 +167,7 @@ const SectionsBrowser = ({ notebook_id }: { notebook_id: number }) => {
           </div>
         )}
       </div>
-    </DndContext>
+    </>
   );
 };
 
